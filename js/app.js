cb5e74cad71b1ef7e9beba5deeced9193efc22e0
Louis Medina <louisfm16@gmail.com>
2019-12-11 19:08:32 -0800

    Implentation of Glide.JS lib (Differs from Glider.js), need to fix perTouch bug where it slides the amount of slides perView instead of perTouch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', function(){
+    var glide = new Glide('#panels-container', {
+        // startAt: 0,
+        perTouch: 1,
+        perView: 3,
+        gap: 200,
+        // focusAt: 0,
 
+        // perView: 1,
+        // peek: 500
+    });
+    
+    glide.mount();
 });
