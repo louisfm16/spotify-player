1c8fa98da3f73151135bb89808795383efa520d4
Louis Medina <louisfm16@gmail.com>
2019-12-11 17:03:06 -0800

    Removed Glider.js and preped for Glide.js lib

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
 window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: "auto",
-        draggable: true,
-        itemWidth: "600px",
-        // exactWidth: true
-    });
+
 });
