98201bb9b1342932e14738e23d01a8d0a135514f
Louis Fernando Medina <louisfm16@gmail.com>
2019-12-13 00:01:18 -0800

    TiltJS alignment

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function(){
+    // Glide JS
     var glide = new Glide('#panels-container', {
         perTouch: 1,
         perView: 3,
@@ -9,4 +10,14 @@ window.addEventListener('load', function(){
     });
     
     glide.mount();
+
+    // Tilt JS
+    VanillaTilt.init(document.querySelectorAll(".curr"), {
+        max: 1,
+        perspective: 300,
+        scale: 1,
+        speed: 300,
+        glare: true,
+
+    });
 });
