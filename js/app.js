4548d6e482856cef4919121cec27335b978793a6
Louis Fernando Medina <louisfm16@gmail.com>
2019-12-15 23:39:25 -0800

    Scrubber + controls + parallax + titl

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', function(){
     // Glide JS
-    var glide = new Glide('#panels-container', {
+    var glide = new Glide('.glide', {
         perTouch: 1,
         perView: 3,
         gap: 100,
+        bound: true,
+        rewind: false,
 
         // perView: 1,
         // peek: 500
@@ -11,13 +13,37 @@ window.addEventListener('load', function(){
     
     glide.mount();
 
-    // Tilt JS
-    VanillaTilt.init(document.querySelectorAll(".curr"), {
-        max: 1,
-        perspective: 300,
-        scale: 1,
-        speed: 300,
-        glare: true,
+    console.log(glide);
 
+    // Tilt JS
+    var curr = document.querySelector("#curr");
+    var img = document.querySelector("#img-curr");
+
+    VanillaTilt.init(curr, {
+        max: 1,
+        perspective: 100,
+        scale: 1,
+        speed: 1500,
+        reverse: true
+    });
+
+    // Parallax
+    curr.addEventListener("tiltChange", function(e) {
+        var mult = 5; 
+
+        var pos = {
+            top: 50 + (e.detail.tiltY * mult),
+            left: 50 - (e.detail.tiltX * mult)
+        }
+
+        img.style.top = (pos.top + "%");
+        img.style.left = (pos.left + "%");
+    });
+
+    // Reset image position
+    // TODO: add transition / Easing
+    curr.addEventListener("mouseleave", function(e) {
+        img.style.top = "50%";
+        img.style.left = "50%";
     });
 });
