74ee59f13ef355fc1706505f613c18fc12997410
Louis Fernando Medina <louisfm16@gmail.com>
2019-12-19 23:14:39 -0800

    Remove Glide lib, prep for swipe

--- js/app.js
+++ js/app.js
@@ -4,59 +4,54 @@ var shuffle = true,
     repeat = false,
     mute = false;
 
-var glide;
-
 window.addEventListener('load', function(){
     Init();
 
-    // Tilt JS
-    var curr = document.querySelector("#curr");
-    var img = document.querySelector("#img-curr");
+    var el = document.getElementById('curr');
+    var ht = Hammer(el);
 
-    VanillaTilt.init(curr, {
-        max: 1,
-        perspective: 100,
-        scale: 1,
-        speed: 1500,
-        reverse: true
+    ht.on('pan', function(e) {
+        console.log("pan: ", e);
     });
 
-    // Parallax
-    curr.addEventListener("tiltChange", function(e) {
-        var mult = 5; 
-
-        var pos = {
-            top: 50 + (e.detail.tiltY * mult),
-            left: 50 - (e.detail.tiltX * mult)
-        }
-
-        img.style.top = (pos.top + "%");
-        img.style.left = (pos.left + "%");
-        img.style.transition = "none";
+    ht.on('press', function(e) {
+        console.log("press: ", e);
     });
 
-    // Reset image position
-    curr.addEventListener("mouseleave", function(e) {
-        img.style.top = "50%";
-        img.style.left = "50%";
-        img.style.transition = "all 0.3s";
-    });
+    // // Tilt JS
+    // var curr = document.querySelector("#curr");
+    // var img = document.querySelector("#img-curr");
+
+    // VanillaTilt.init(curr, {
+    //     max: 1,
+    //     perspective: 100,
+    //     scale: 1,
+    //     speed: 1500,
+    //     reverse: true
+    // });
+
+    // // Parallax
+    // curr.addEventListener("tiltChange", function(e) {
+    //     var mult = 5; 
+
+    //     var pos = {
+    //         top: 50 + (e.detail.tiltY * mult),
+    //         left: 50 - (e.detail.tiltX * mult)
+    //     }
+
+    //     img.style.top = (pos.top + "%");
+    //     img.style.left = (pos.left + "%");
+    //     img.style.transition = "none";
+    // });
+
+    // // Reset image position
+    // curr.addEventListener("mouseleave", function(e) {
+    //     img.style.top = "50%";
+    //     img.style.left = "50%";
+    //     img.style.transition = "all 0.3s";
+    // });
 });
 
 function Init() {
-    // Glide JS
-    // TODO: May need to reference later for breakpoints
-    glide = new Glide('.glide', {
-        startAt: 1,
-        perTouch: 1,
-        perView: 1,
-        gap: 200,
-        bound: true,
-        rewind: false,
-        peek: 900
-    }).mount();
-
 
 }
-
-// function ad
