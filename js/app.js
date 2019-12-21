46bf177902fc5bff64b709eea650caf630edc106
Louis Fernando Medina <louisfm16@gmail.com>
2019-12-20 17:51:50 -0800

    Panning + set up for other features and events

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 // Flags
-var shuffle = true,
-    pause = false,
-    repeat = false,
+var pause = false,
     mute = false;
+
+var mc,
+    screen,
+    currCard,
+    currCardPos = {};
+
+const bp = 300,
+    angleBp = 10,
+    lAngleBp = 180 - angleBp,
+    rAngleBp = angleBp;
 
 window.addEventListener('load', function(){
     Init();
 
-    var el = document.getElementById('curr');
-    var ht = Hammer(el);
-
-    ht.on('pan', function(e) {
-        console.log("pan: ", e);
+    mc.on('press panstart panmove', function(e) {
+        // TODO: add smooth scale animation, problems with transition
+        UpdateCardPos(currCard, e);
     });
 
-    ht.on('press', function(e) {
-        console.log("press: ", e);
+    mc.on('pressup panend', function(e) {
+        var dist = Math.abs(e.deltaX);
+        var dir = Math.sign(e.deltaX);
+
+        // Do we take action or snap back
+        if(dist >= bp) {
+            SnapAnimation(currCard, e);
+
+            if(dir > 0) { // Right
+                SnapRight(currCard);
+            }
+            else if(dir < 0) { // Left
+                SnapLeft(currCard);
+            }
+        }
+        else {
+            SnapBack(currCard);
+        }
     });
 
     // // Tilt JS
@@ -53,5 +75,56 @@ window.addEventListener('load', function(){
 });
 
 function Init() {
+    screen = document.getElementById('panels-container');
+    currCard = document.getElementById('curr');
+    mc = new Hammer.Manager(currCard);
+    mc.add(new Hammer.Pan());
+    mc.add(new Hammer.Press());
+}
+
+function UpdateCurrCard() {
+
+}
+
+function UpdateCardPos(el, e) {
+    currCardPos = {
+        x: (e.deltaX - (el.offsetWidth / 2)),
+        y: (e.deltaY - (el.offsetHeight / 2))
+    }
+
+    el.style.transform = `translate(${currCardPos.x}px, ${currCardPos.y}px) scale(1.1)`;
+    el.style.transition = 'none';
+}
+
+// Actions
+function SnapAnimation(el, e) {
+    var angle = Math.abs(e.angle);
+    var newPos = {
+        x: ((screen.offsetWidth / 2) * Math.sign(e.deltaX) + currCardPos.x),
+        y: ((screen.offsetHeight / 2) * Math.sign(e.deltaY) + currCardPos.y)
+    };
+
+    // Check to see if we have to animate diagonally
+    if(angle >= (lAngleBp) || angle <= rAngleBp) {
+        newPos.y = currCardPos.y;
+    }
+
+    el.style.transform = `translate(${newPos.x}px, ${newPos.y}px)`;
+    el.style.transition = 'all 0.2s ease-in-out';
+}
+
+function SnapBack(el) {
+    // Original absolute center position
+    el.style.top = '50%';
+    el.style.left = '50%';
+    el.style.transform = 'translate(-50%, -50%)';
+    el.style.transition = 'all 0.3s ease-in-out';
+}
+
+function SnapRight(el) {
+    
+}
+
+function SnapLeft(el) {
 
 }
